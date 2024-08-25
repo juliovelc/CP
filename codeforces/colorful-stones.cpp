41439d1c5d8590ab030eeefae368a9f0f5179c2f
Julio Velazquez <jalejxndro@gmail.com>
2024-08-25 01:02:03 -0600

    added solutions for different problems in codeforces

---
+++ codeforces/colorful-stones.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+
+  int pos = 0;
+  for (int i = 0; i < t.size(); i++) {
+    if (s[pos] == t[i])
+      pos++;
+  }
+  cout << pos + 1 << endl;
+}
